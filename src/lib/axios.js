292d87d6b1c7b2f0d8324d6ded92a260f3c8b268
Ramvishwajithin Porandla <ramvishwajithin@gmail.com>
2025-08-24 18:38:48 +0530

    second commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api/companies",
+  baseURL: "https://companies-backend-omega.vercel.app/api/companies",
   withCredentials: true,
 });
+
